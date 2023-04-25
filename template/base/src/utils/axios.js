4080b4668e6d2cfff66a5d4882dc42c3f14378be
laqudee <2268678583@qq.com>
2023-04-25 21:14:33 +0800

    fix some bug of template utils axios config

--- template/base/src/utils/axios.js
+++ template/base/src/utils/axios.js
@@ -1,11 +1,13 @@
 import { createAxios } from 'common-toolbox/src/index.js'
-import { getAccessToken } from './cookie.js'
-import router from '../router'
+import { getToken } from './cookie.js'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
 
 const axios = createAxios({
   baseURL: '',
   timeout: 50000,
-  getAccessToken,
+  getToken,
   router
 })
 
